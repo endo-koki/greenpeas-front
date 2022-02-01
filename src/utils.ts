735b36bb131c2d05c6383e1122dfb13ff4801be5
endo-koki <endo-k-88st0@g.ecc.u-tokyo.ac.jp>
2022-02-01 19:47:02 +0900

    change apiRoot to replit

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 // global var
 export const margin: number = 10;
 
-export const apiRoot: string = 'http://localhost:5000';
+export const apiRoot: string = 'https://GreenPeas.rendob.repl.co';
 
 /** calculate table width. (table width = cell width + border width * 2) */
 export function calcTableWidth(
